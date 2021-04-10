efa244d8226c559f2c5b4f5a3ff22d4a3012d9aa
Mounica Sykam <mounica.sykam@gmail.com>
2021-04-10 17:11:04 -0400

    first commit

---
+++ js/project.js
@@ -0,0 +1,48 @@
+function displayDescriptionAss1() {
+	document.getElementById("proj-desc-Ass1").style.display = "block";
+	document.getElementById("proj-desc-Ass2").style.display = "none";
+	document.getElementById("proj-desc-Ass3").style.display = "none";
+	document.getElementById("proj-desc-Ass4").style.display = "none";
+	document.getElementById("proj-desc-Ass5").style.display = "none";
+	document.getElementById("proj-desc-Ass6").style.display = "none";
+}
+function displayDescriptionAss2() {
+	document.getElementById("proj-desc-Ass1").style.display = "none";
+	document.getElementById("proj-desc-Ass2").style.display = "block";
+	document.getElementById("proj-desc-Ass3").style.display = "none";
+	document.getElementById("proj-desc-Ass4").style.display = "none";
+	document.getElementById("proj-desc-Ass5").style.display = "none";
+	document.getElementById("proj-desc-Ass6").style.display = "none";
+}
+function displayDescriptionAss3() {
+	document.getElementById("proj-desc-Ass1").style.display = "none";
+	document.getElementById("proj-desc-Ass2").style.display = "none";
+	document.getElementById("proj-desc-Ass3").style.display = "block";
+	document.getElementById("proj-desc-Ass4").style.display = "none";
+	document.getElementById("proj-desc-Ass5").style.display = "none";
+	document.getElementById("proj-desc-Ass6").style.display = "none";
+}
+function displayDescriptionAss4() {
+	document.getElementById("proj-desc-Ass1").style.display = "none";
+	document.getElementById("proj-desc-Ass2").style.display = "none";
+	document.getElementById("proj-desc-Ass3").style.display = "none";
+	document.getElementById("proj-desc-Ass4").style.display = "block";
+	document.getElementById("proj-desc-Ass5").style.display = "none";
+	document.getElementById("proj-desc-Ass6").style.display = "none";
+}
+function displayDescriptionAss5() {
+	document.getElementById("proj-desc-Ass1").style.display = "none";
+	document.getElementById("proj-desc-Ass2").style.display = "none";
+	document.getElementById("proj-desc-Ass3").style.display = "none";
+	document.getElementById("proj-desc-Ass4").style.display = "none";
+	document.getElementById("proj-desc-Ass5").style.display = "block";
+	document.getElementById("proj-desc-Ass6").style.display = "none";
+}
+function displayDescriptionAss6() {
+	document.getElementById("proj-desc-Ass1").style.display = "none";
+	document.getElementById("proj-desc-Ass2").style.display = "none";
+	document.getElementById("proj-desc-Ass3").style.display = "none";
+	document.getElementById("proj-desc-Ass4").style.display = "none";
+	document.getElementById("proj-desc-Ass5").style.display = "block";
+	document.getElementById("proj-desc-Ass6").style.display = "none";
+}
